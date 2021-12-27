1c6e032b5e269a0173dc63b819ce30700c84b7ae
xeype <alexeypedko@gmail.com>
2021-12-27 17:29:11 +0200

    update const.py

--- bot.py
+++ bot.py
@@ -13,31 +13,31 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('Add place', 'Show my list')
-    if message.text == '/start':
-        bot.send_message(message.chat.id, const.start_command, reply_markup=keyboard, parse_mode='markdown')
+    keyboard.row(const.ADD_PLACE, const.SHOW_LIST)
+    if message.text == const.START_COMMAND:
+        bot.send_message(message.chat.id, const.START_OUTPUT, reply_markup=keyboard, parse_mode='markdown')
 
         us_id = message.from_user.id
         username = message.from_user.username
 
         dbHandler.db_table_val(us_id, username)
-    elif message.text == '/help':
-        bot.send_message(message.chat.id, 'How can i help u?', reply_markup=keyboard)
+    elif message.text == const.HELP_COMMAND:
+        bot.send_message(message.chat.id, const.HELP_OUTPUT, reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['add', 'list'])
 def handle_add_list(message):
-    if message.text == '/add':
+    if message.text == const.ADD_COMMAND:
         bot.send_message(message.chat.id, 'Add place', parse_mode='markdown')
-    elif message.text == '/list':
+    elif message.text == const.LIST_COMMAND:
         bot.send_message(message.chat.id, 'My places', parse_mode='markdown')
 
 
 @bot.message_handler(content_types='text')
 def handle_buttons(message):
-    if message.text == 'Add place':
+    if message.text == const.ADD_PLACE:
         bot.send_message(message.chat.id, 'Add place', parse_mode='markdown')
-    elif message.text == 'Show my list':
+    elif message.text == const.SHOW_LIST:
         bot.send_message(message.chat.id, 'My places', parse_mode='markdown')
 
 
--- const.py
+++ const.py
@@ -1,1 +1,13 @@
-start_command = "Hello!\nThis is _My Places bot_.\n\nYou can add here places that you visited and rate them."
+# Commands
+START_COMMAND = "/start"
+HELP_COMMAND = "/help"
+ADD_COMMAND = "/add"
+LIST_COMMAND = "/list"
+
+# Buttons
+ADD_PLACE = "Add place"
+SHOW_LIST = "Show my list"
+
+# OUTPUTS
+START_OUTPUT = "Hello!\nThis is _My Places bot_.\n\nYou can add here places that you visited and rate them."
+HELP_OUTPUT = "How can i help you?"
