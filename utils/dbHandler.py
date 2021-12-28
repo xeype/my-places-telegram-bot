a8ffe26bd2c126b8b070167aad1ced18e71d3008
xeype <alexeypedko@gmail.com>
2021-12-28 12:47:22 +0200

    add execute method to add place

--- utils/dbHandler.py
+++ utils/dbHandler.py
@@ -1,5 +1,5 @@
 import sqlite3
-import queries
+from utils import queries
 
 conn = sqlite3.connect('db/db.db', check_same_thread=False)
 cursor = conn.cursor()
@@ -7,4 +7,9 @@ cursor = conn.cursor()
 
 def db_table_val(user_id: int, username: str):
     cursor.execute(queries.add_user, (user_id, username))
+    conn.commit()
+
+
+def add_place(user_id: int, place_name: str, rating: int):
+    cursor.execute(queries.add_place, (user_id, place_name, rating))
     conn.commit()
