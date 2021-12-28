ed72d567171ed05f6a4fc299325ef3b47b007b21
xeype <alexeypedko@gmail.com>
2021-12-28 12:47:44 +0200

    add query to add place

--- utils/queries.py
+++ utils/queries.py
@@ -1,1 +1,3 @@
 add_user = "INSERT INTO users (user_id, username) VALUES (?, ?)"
+add_place = "INSERT INTO places (user_id, place_name, rating) " \
+            "VALUES ((SELECT users.id FROM users WHERE users.user_id = (?)), ?, ?)"
