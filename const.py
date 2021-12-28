592b9cec4ea2bf343c18e8059224252e11d63587
xeype <alexeypedko@gmail.com>
2021-12-28 12:48:00 +0200

    update const.py

--- const.py
+++ const.py
@@ -10,4 +10,6 @@ SHOW_LIST = "Show my list"
 
 # OUTPUTS
 START_OUTPUT = "Hello!\nThis is _My Places bot_.\n\nYou can add here places that you visited and rate them."
-HELP_OUTPUT = "How can i help you?"
+HELP_OUTPUT = "*Commands*\n\n/help - Commands description\n" \
+              "/add - Add new place\n/list - Show all your places"
+UNKNOWN_OUTPUT = "I don't know this command\nEnter /help to see the commands"
