b2ba68ffc38bfe5429aecc324fdc8f937c95986b
xeype <alexeypedko@gmail.com>
2021-12-28 14:47:39 +0200

    add query to get places

--- utils/queries.py
+++ utils/queries.py
@@ -1,3 +1,5 @@
 add_user = "INSERT INTO users (user_id, username) VALUES (?, ?)"
 add_place = "INSERT INTO places (user_id, place_name, rating, place_desc) " \
             "VALUES ((SELECT users.id FROM users WHERE users.user_id = (?)), ?, ?, ?)"
+get_places = f"SELECT place_name, rating, place_desc FROM places " \
+             "WHERE user_id = (SELECT users.id FROM users WHERE users.user_id = {})  ORDER BY rating DESC"
